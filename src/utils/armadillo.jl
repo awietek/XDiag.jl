28332b75b0ee74053299029c01dfa12ef3b22953
Alexander Wietek <awietek@pks.mpg.de>
2024-08-12 14:43:33 +0200

    changed complex pointers to float64 pointers in armadillo conversion

--- src/utils/armadillo.jl
+++ src/utils/armadillo.jl
@@ -27,9 +27,9 @@ function to_julia(vec::cxx_arma_cx_vec)
     julia_vec = Vector{ComplexF64}(undef, m)
 
     # Low level C call to copy
-    vec_ptr = reinterpret(Ptr{ComplexF64}, memptr(vec))
-    julia_vec_ptr = Base.unsafe_convert(Ptr{ComplexF64}, julia_vec)
-    Base.unsafe_copyto!(julia_vec_ptr, vec_ptr, m)
+    vec_ptr = reinterpret(Ptr{Float64}, memptr(vec))
+    julia_vec_ptr = Base.unsafe_convert(Ptr{Float64}, julia_vec)
+    Base.unsafe_copyto!(julia_vec_ptr, vec_ptr, 2*m)
     return julia_vec
 end
 
@@ -53,8 +53,8 @@ function to_julia(mat::cxx_arma_cx_mat)
     julia_mat = Matrix{ComplexF64}(undef, m, n)
 
     # Low level C call to copy
-    mat_ptr = reinterpret(Ptr{ComplexF64}, memptr(mat))
-    julia_mat_ptr = Base.unsafe_convert(Ptr{ComplexF64}, julia_mat)
-    Base.unsafe_copyto!(julia_mat_ptr, mat_ptr, N)
+    mat_ptr = reinterpret(Ptr{Float64}, memptr(mat))
+    julia_mat_ptr = Base.unsafe_convert(Ptr{Float64}, julia_mat)
+    Base.unsafe_copyto!(julia_mat_ptr, mat_ptr, 2*N)
     return julia_mat
 end
