2d03beb91cdb05875b1370babc2fca9123a88b88
Alexander Wietek <awietek@pks.mpg.de>
2024-09-10 10:05:20 +0200

    Corrected kwargs in states

--- src/states/create_state.jl
+++ src/states/create_state.jl
@@ -1,9 +1,9 @@
-product(block::Block, local_states::Vector{String}, real::Bool=true) =  State(cxx_product(block.cxx_block, StdVector(StdString.(local_states)), real))
+product(block::Block, local_states::Vector{String}; real::Bool=true) =  State(cxx_product(block.cxx_block, StdVector(StdString.(local_states)), real))
 
-Base.rand(block::Block, real::Bool=true, seed::Int64=42, normalized::Bool=true) =
+Base.rand(block::Block; real::Bool=true, seed::Int64=42, normalized::Bool=true) =
     State(cxx_rand(block.cxx_block, real, seed, normalized))
 
-Base.zeros(block::Block, real::Bool=true, n_col::Int64=1) =
+Base.zeros(block::Block; real::Bool=true, n_col::Int64=1) =
     State(cxx_zeros(block.cxx_block, real, n_col))
 
 Base.zero(state::State) = cxx_zero(state.cxx_state)
