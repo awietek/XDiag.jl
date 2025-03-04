33035d10f173c4b9d8bd21a045c8e5a8f4629b9b
mulaga <martinu@pks.mpg.de>
2025-03-04 14:58:36 +0100

    .

--- test/symmetries/symmetries.jl
+++ test/symmetries/symmetries.jl
@@ -2,25 +2,21 @@ using XDiag
 using Test
 using Random
 
-@testset "symmetries.jl" begin
-
     @testset "symmetries.jl" begin
-        # multiplication, identity, inverse, power
-        for n_sites = 1:8
-            id = Permutation(n_sites)
-            randomPerm = Permutation(Random.shuffle(Xoshiro(42), collect(1:n_sites)))
-            randomInv = inv(randomPerm)
-            @test id*randomPerm == randomPerm
-            @test randomPerm*id == randomPerm
-            @test randomPerm*randomInv == id
-            @test randomInv*randomPerm == id
-            @test randomPerm ^ 2 == randomPerm*randomPerm
-        end
-
-        # permutation group?
-
-        # representation?
-        
-
+    # multiplication, identity, inverse, power
+    for n_sites = 1:8
+        id = Permutation(n_sites)
+        randomPerm = Permutation(Random.shuffle(Xoshiro(42), collect(1:n_sites)))
+        randomInv = inv(randomPerm)
+        @test id*randomPerm == randomPerm
+        @test randomPerm*id == randomPerm
+        @test randomPerm*randomInv == id
+        @test randomInv*randomPerm == id
+        @test randomPerm ^ 2 == randomPerm*randomPerm
     end
+
+    # permutation group?
+
+    # representation?
+
 end
